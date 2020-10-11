749d865821bf8717550e665e7b677b8120accd46
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-10-11 09:54:13 +0200

    Add CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const { v4: uuid } = require('uuid')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const todos = [
     {
         id: uuid(),
         description: 'Create mandatory project 1',
-        done: false,
+        completed: false,
     },
 ]
 
 app.get('/todos', (request, response) => {
-    // response.send()
+    response.send({ data: todos })
 })
 
 const port = process.env.PORT || 8081
